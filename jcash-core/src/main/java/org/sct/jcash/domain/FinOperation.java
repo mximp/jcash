fac34929638f9dd109d91e7d9c6e7a246abed183
mpetrov <mpetrov@raccoonsoft.ru>
2020-12-02 19:43:36 +0300

    Operation entity added

--- jcash-core/src/main/java/org/sct/jcash/domain/FinOperation.java
+++ jcash-core/src/main/java/org/sct/jcash/domain/FinOperation.java
@@ -1,18 +1,38 @@
 package org.sct.jcash.domain;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 public class FinOperation {
     private final double amount;
     private final LocalDateTime date;
-    private String account;
-    private int sign;
+    private final String account;
+    private final Set<String> classifiers = new HashSet<>();
 
-    public FinOperation(double amount, LocalDateTime date, String account, int sign) {
+
+    private FinOperation(double amount, LocalDateTime date, String account) {
         this.amount = amount;
         this.date = date;
         this.account = account;
-        this.sign = sign;
+    }
+
+    public static FinOperation of(double amount, LocalDateTime date, String account) {
+        if(date == null) {
+            throw new IllegalArgumentException("Operation date must be provided");
+        }
+
+        if(account == null) {
+            throw new IllegalArgumentException("Operation account must be provided");
+        }
+
+        return new FinOperation(amount, date, account);
+    }
+
+    public static FinOperation of(double amount, String account) {
+        return of(amount, LocalDateTime.now(), account);
     }
 
     public double getAmount() {
@@ -27,7 +47,25 @@ public class FinOperation {
         return account;
     }
 
-    public int getSign() {
-        return sign;
+    @Override
+    public String toString() {
+        return "FinOperation{" +
+                "amount=" + amount +
+                ", date=" + date +
+                ", account='" + account + '\'' +
+                ", classifiers=" + classifiers +
+                '}';
+    }
+
+    public void classify(Collection<String> classifiers) {
+        this.classifiers.addAll(classifiers);
+    }
+
+    public Set<String> getClassifiers() {
+        return Collections.unmodifiableSet(classifiers);
+    }
+
+    public void removeClassifier(String classifier) {
+        this.classifiers.remove(classifier);
     }
 }
--- jcash-core/src/test/java/org/sct/jcash/domain/OperationTest.java
+++ jcash-core/src/test/java/org/sct/jcash/domain/OperationTest.java
@@ -4,23 +4,78 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
 
 public class OperationTest {
 
     @Test
-    public void createExpenseOperation() {
-
+    public void createOperation() {
+        // given
         var date = LocalDateTime.now();
-        var amount = 100.0;
+        var amount = -100.0;
         var account = "AC#1";
-        var sign = -1;
 
-        FinOperation expense = new FinOperation(amount, date, account, sign);
+        // when
+        FinOperation expense = FinOperation.of(amount, date, account);
 
+        // then
+        System.out.println(expense);
         Assertions.assertEquals(amount, expense.getAmount());
         Assertions.assertEquals(date, expense.getDate());
         Assertions.assertEquals(account, expense.getAccount());
-        Assertions.assertEquals(sign, expense.getSign());
+    }
+
+    @Test
+    public void assignClassifiers() {
+        var expense = FinOperation.of(-100.0, "AC#1");
+        var classifiers = Set.of("A", "B");
+
+        expense.classify(classifiers);
+
+        Assertions.assertTrue(Set.of("B", "A").containsAll(expense.getClassifiers()));
+    }
+
+    @Test
+    public void assignAdditionalClassifiers() {
+        var expense = FinOperation.of(-100.0, "AC#1");
+        expense.classify(Set.of("A")); // already has classifiers
+
+        expense.classify(Set.of("C", "B"));
+
+        Assertions.assertTrue(expense.getClassifiers().containsAll(Set.of("A", "B", "C")));
+    }
+
+    @Test
+    public void removeClassifier() {
+        var expense = FinOperation.of(-100.0, "AC#1");
+        expense.classify(List.of("A", "B", "C"));
+
+        expense.removeClassifier("B");
+
+        Assertions.assertTrue(expense.getClassifiers().containsAll(Set.of("A", "C")));
+        Assertions.assertFalse(expense.getClassifiers().contains("B"));
+    }
+
+    @Test
+    public void removeNonExistingClassifier() {
+        var expense = FinOperation.of(-100.0, "AC#1");
+        expense.classify(List.of("A", "B", "C"));
+
+        expense.removeClassifier("D");
+
+        Assertions.assertTrue(expense.getClassifiers().containsAll(Set.of("A", "B", "C")));
+    }
+
+    @Test
+    public void missingArguments() {
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> FinOperation.of(100, null, "AC#1")
+        );
+
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> FinOperation.of(100, LocalDateTime.now(), null)
+        );
     }
 
 }
